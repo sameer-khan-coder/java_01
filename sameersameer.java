7f496c14453285581e1ce4bf6f862ba29c5f2ea5
sameer-khan <73671213+sameer-khan-coder@users.noreply.github.com>
2022-10-07 15:27:26 +0530

    Add files via upload

---
+++ sameersameer.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+public class sameersameer{
+
+  public static void main(String[] args) {
+
+  int val=9/2;
+   double sk=10d/6d;
+   int marker=512;
+  double percentage =marker*0.46f;
+  System.out.println(percentage);  
+  char var1='sdd';
+  System.out.println(var1); 
+}
+}
